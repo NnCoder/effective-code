dc42d4c7a43290cae212d50ebb5aa20edbdf3c84
LittleTao <1419966866@qq.com>
2023-02-17 09:54:34 +0800

    新增spel解析方法属性代码

---
+++ SpELKeyProvider.java
@@ -0,0 +1,84 @@
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.context.expression.MethodBasedEvaluationContext;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 解析方法中属性值，可用于redis锁等场景
+ * 例:
+ * @RedisLock("#user.userId")
+ * void method(User user)
+ */
+public class SpELKeyProvider {
+
+    private ParameterNameDiscoverer nameDiscoverer = new DefaultParameterNameDiscoverer();
+
+    private ExpressionParser parser = new SpelExpressionParser();
+
+    public String getKeyName(ProceedingJoinPoint joinPoint, RedisLock lock) {
+        List<String> keyList = new ArrayList<>();
+        Method method = getMethod(joinPoint);
+        List<String> definitionKeys = getSpelDefinitionKey(lock.keys(), method, joinPoint.getArgs());
+        keyList.addAll(definitionKeys);
+        List<String> parameterKeys = getParameterKey(method.getParameters(), joinPoint.getArgs());
+        keyList.addAll(parameterKeys);
+        return StringUtils.collectionToDelimitedString(keyList,"","-","");
+    }
+
+    private Method getMethod(ProceedingJoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        if (method.getDeclaringClass().isInterface()) {
+            try {
+                method = joinPoint.getTarget().getClass().getDeclaredMethod(signature.getName(),
+                        method.getParameterTypes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return method;
+    }
+
+    private List<String> getSpelDefinitionKey(String[] definitionKeys, Method method, Object[] parameterValues) {
+        List<String> definitionKeyList = new ArrayList<>();
+        for (String definitionKey : definitionKeys) {
+            if (definitionKey != null && !definitionKey.isEmpty()) {
+                EvaluationContext context = new MethodBasedEvaluationContext(null, method, parameterValues, nameDiscoverer);
+                Object value = parser.parseExpression(definitionKey).getValue(context);
+                String key = Optional.ofNullable(value).orElse("").toString();
+                definitionKeyList.add(key);
+            }
+        }
+        return definitionKeyList;
+    }
+
+    private List<String> getParameterKey(Parameter[] parameters, Object[] parameterValues) {
+        List<String> parameterKey = new ArrayList<>();
+        for (int i = 0; i < parameters.length; i++) {
+            if (parameters[i].getAnnotation(RedisLockKey.class) != null) {
+                RedisLockKey keyAnnotation = parameters[i].getAnnotation(RedisLockKey.class);
+                if (keyAnnotation.value().isEmpty()) {
+                    parameterKey.add(parameterValues[i].toString());
+                } else {
+                    StandardEvaluationContext context = new StandardEvaluationContext(parameterValues[i]);
+                    Object value = parser.parseExpression(keyAnnotation.value()).getValue(context);
+                    String key = Optional.ofNullable(value).orElse("").toString();
+                    parameterKey.add(key);
+                }
+            }
+        }
+        return parameterKey;
+    }
+}
